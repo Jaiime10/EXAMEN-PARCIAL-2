95c9829d60c959528f44d9791e8b48f69d978f2c
Jaiime10 <jcanaman@myuax.com>
2024-11-05 11:09:56 +0100

    Examen

--- EJERCICIO 4/promocion.cpp
+++ EJERCICIO 4/promocion.cpp
@@ -3,3 +3,24 @@
 //
 
 #include "promocion.h"
+
+#include <iostream>
+
+
+using namespace std;
+
+// Implementación de la función que calcula el total a pagar bajo la promoción
+double calcularTotalConPromocion(double precio1, double precio2, double precio3) {
+    // Encontrar el artículo más barato
+    double precioMinimo = precio1;
+
+    if (precio2 < precioMinimo) {
+        precioMinimo = precio2;
+    }
+    if (precio3 < precioMinimo) {
+        precioMinimo = precio3;
+    }
+
+    // Sumar los precios de los dos artículos más caros (descontando el más barato)
+    return (precio1 + precio2 + precio3) - precioMinimo;
+}
--- EJERCICIO 4/promocion.h
+++ EJERCICIO 4/promocion.h
@@ -5,12 +5,7 @@
 #ifndef PROMOCION_H
 #define PROMOCION_H
 
+// Declaración de la función que calcula el total a pagar bajo la promoción
+double calcularTotalConPromocion(double precio1, double precio2, double precio3);
 
-
-class promocion {
-
-};
-
-
-
-#endif //PROMOCION_H
+#endif // PROMOCION_H
