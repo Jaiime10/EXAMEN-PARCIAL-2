cfe996a5ea32e316fbbb436d2a8d1f416d836db6
Jaiime10 <jcanaman@myuax.com>
2024-11-05 10:45:46 +0100

    Examen

---
+++ EJERCICIO 1/numeros.h
@@ -0,0 +1,16 @@
+//
+// Created by canas on 05/11/2024.
+//
+
+#ifndef NUMEROS_H
+#define NUMEROS_H
+
+
+
+class numeros {
+
+};
+
+
+
+#endif //NUMEROS_H
---
+++ EJERCICIO 2/semana.h
@@ -0,0 +1,16 @@
+//
+// Created by canas on 05/11/2024.
+//
+
+#ifndef SEMANA_H
+#define SEMANA_H
+
+
+
+class semana {
+
+};
+
+
+
+#endif //SEMANA_H
