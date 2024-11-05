48712f17e70c00c22b0ebed36057bbc58c791b58
Jaiime10 <jcanaman@myuax.com>
2024-11-05 10:48:45 +0100

    Examen

--- EJERCICIO 1/numeros.cpp
+++ EJERCICIO 1/numeros.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "numeros.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numero;
+
+    // Solicitar al usuario que ingrese un número entero
+    cout << "Ingresa un número entero: ";
+    cin >> numero;
+
+    // Analizar el número ingresado y determinar si es positivo, negativo o cero
+    if (numero > 0) {
+        cout << "El número es positivo." << endl;
+    } else if (numero < 0) {
+        cout << "El número es negativo." << endl;
+    } else {
+        cout << "El número es cero." << endl;
+    }
+
+    return 0;
+}
+
